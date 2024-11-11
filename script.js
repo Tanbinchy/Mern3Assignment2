640b38a4343ae1b04f4fdd7cace9e99b475bef2a
Tanbin Chy <tanbinchy@gmail.com>
2024-11-11 20:29:17 +0600

    Script Position Changed

---
+++ script.js
@@ -0,0 +1,16 @@
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+
+::-webkit-scrollbar {
+  width: 1.5rem;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: var(--main-color);
+}
+
+::-webkit-scrollbar-track {
+  background-color: var(--bg-color);
+}
